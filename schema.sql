260a76d1e3a1c32500f755f409ffc15c11c8df7f
Justin Forest <hex@umonkey.net>
2026-01-29 03:36:02 +0400

    Add database filestorage

--- schema.sql
+++ schema.sql
@@ -1,3 +1,7 @@
 CREATE DATABASE IF NOT EXISTS sebmus;
 CREATE USER IF NOT EXISTS 'sebmus'@'%' IDENTIFIED BY 'ecbuPyfV';
 GRANT ALL PRIVILEGES ON sebmus.* TO 'sebmus'@'%';
+
+CREATE DATABASE IF NOT EXISTS filestorage;
+CREATE USER IF NOT EXISTS 'filestorage'@'%' IDENTIFIED BY 'filestorage';
+GRANT ALL PRIVILEGES ON filestorage.* TO 'filestorage'@'%';
